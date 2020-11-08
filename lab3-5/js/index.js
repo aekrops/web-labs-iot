f4d174eb6fcf1b2edc88f718737693ce8ed7d17c
aekrops <svyatoslavtrostynski24@gmail.com>
2020-11-08 17:23:15 +0200

    update code

--- lab3-5/js/index.js
+++ lab3-5/js/index.js
@@ -12,8 +12,9 @@ const pricePerHourInDollars__input = document.getElementById('pricePerHourInDoll
 const lawyersHtmlWrapper = document.getElementById('lawyers__list');
 var priceCount;
 var listOfLawyer = [];
-let lawyersFinded;
+let lawyersFinded = [];
 var keys;
+var id;
 
 setup();
 
@@ -34,8 +35,8 @@ function loadSortedFirebase() {
 }
 
 function loadFirebaseToSort() {
-    var ref = database.ref("lawyers");
-    ref.on("value", readData, errData);
+    var firebaseRef = database.ref("lawyers");
+    firebaseRef.on("value", readData, errData);
 
 }
 
@@ -52,14 +53,17 @@ function readData(data) {
 function getData(data) {
     var lawyers = data.val();
     let innerItem = ` `;
-
+    lawyersFinded = []
     priceCount = 0;
-
+    id = 0;
+    cleanPage();
     keys = Object.keys(data.val());
     keys.forEach((lawyer, index) => {
         key = keys[index];
         lawyer = lawyers[key];
-        priceCount += Number.parseInt(lawyer.pricePerHourInDollars);
+        if (!isNaN(lawyer.pricePerHourInDollars)) {
+            priceCount += Number.parseInt(lawyer.pricePerHourInDollars);
+        }
         listOfLawyer.push(lawyer);
         innerItem += `
         <li class="main_card-item">
@@ -83,16 +87,17 @@ function getData(data) {
                     </div>
                         
                     <div class="card_btn_wrapper">
-                        <button id="btnEdit" class="btn__edit" onclick="editLawyer(${index})" type="button">
+                        <button id="btnEdit" class="btn__edit" onclick="editLawyer(${id})" type="button">
                             Edit
                         </button>
-                        <button id="btnDelete" class="btn__del" onclick="deleteLawyer(${index})"  type="button">
+                        <button id="btnDelete" class="btn__del" onclick="deleteLawyer(${id})"  type="button">
                             Delete
                         </button>
                     </div>
                 </a>
             </li>
         `;
+        id++;
     });
     lawyersHtmlWrapper.innerHTML = innerItem;
     sumOfPrice.innerText = priceCount;
@@ -127,10 +132,10 @@ const displayLawyers = (lawyers) => {
                     </div>
                         
                     <div class="card_btn_wrapper">
-                        <button id="btnEdit" class="btn__edit" onclick="editLawyer()" type="button">
+                        <button id="btnEdit" class="btn__edit" onclick="editLawyer(${lawyer.id})" type="button">
                             Edit
                         </button>
-                        <button id="btnDelete" class="btn__del" onclick="deleteLawyer()"  type="button">
+                        <button id="btnDelete" class="btn__del" onclick="deleteLawyer(${lawyer.id})"  type="button">
                             Delete
                         </button>
                     </div>
@@ -139,6 +144,7 @@ const displayLawyers = (lawyers) => {
     `
     }).join('');
     lawyersHtmlWrapper.innerHTML = displayItems;
+    sumOfPrice.innerText = priceCount;
 }
 
 function cleanPage() {
@@ -149,6 +155,7 @@ function cleanPage() {
 function sortData() {
     if (chkbox.checked) {
         listOfLawyer = []
+        lawyersFinded = []
         loadFirebaseToSort();
         cleanPage();
         listOfLawyer.sort(priceSort);
@@ -156,7 +163,8 @@ function sortData() {
 
     } else {
         listOfLawyer = []
-        loadFirebaseToSort();
+        lawyersFinded = []
+        loadFirebase();
         cleanPage();
         displayLawyers(listOfLawyer);
 
@@ -164,33 +172,36 @@ function sortData() {
 
 }
 
-function priceSort(priceF, priceS) {
-    return priceF.pricePerHourInDollars - priceS.pricePerHourInDollars;
+function priceSort(priceA, priceB) {
+    return priceB.pricePerHourInDollars - priceA.pricePerHourInDollars;
 }
 
-function createLawyer() {
+function writeData() {
     var data = {
         firstName: firstName.value,
         secondName: secondName.value,
         age: age.value,
-        pricePerHourInDollars: pricePerHourInDollars.value
+        pricePerHourInDollars: pricePerHourInDollars.value,
+        id: id
     }
-    if (validation() === false) {
-        lawyersManager = database.ref('lawyers').push(data, finished);
+    if (validation()) {
+        lawyersManager = database.ref('lawyers').push(data, savedData);
     } else {
-        alert("Empty fields!");
+        console.log("Validation error!");
     }
 
     cleanInputs();
 
-    function finished(err) {
+    function savedData(err) {
         if (err) {
             console.log("Something went wrong.");
             console.log(err);
         } else {
-            console.log('Data saved successfully');
+            console.log('Data saved');
         }
     }
+    listOfLawyer = [];
+    loadFirebase();
 }
 
 function cleanInputs() {
@@ -201,60 +212,96 @@ function cleanInputs() {
 }
 
 function validation() {
-    if (firstName__input.value == "") {
-        alert("Enter first name");
-        return true;
-    }
-    if (secondName__input.value == "") {
-        alert("Enter second name");
-        return true;
-    }
-    if (age__input.value == "") {
-        alert("Enter age");
-        return true;
-    }
-    if (pricePerHourInDollars__input.value == "") {
-        alert("Enter price");
-        return true;
-    } else {
+    if (firstName__input.value == "" && secondName__input.value == "" && age__input.value == "" && pricePerHourInDollars__input.value == "") {
+        alert("Enter inputs area")
         return false;
+    } else if (secondName__input.value == "" && age__input.value == "" && pricePerHourInDollars__input.value == "") {
+        alert("Enter second name, age, price");
+        return false;
+    } else if (firstName__input.value == "" && age__input.value == "" && pricePerHourInDollars__input.value == "") {
+        alert("Enter first name, age, price");
+        return false;
+    } else if (firstName__input.value == "" && secondName__input.value == "" && pricePerHourInDollars__input.value == "") {
+        alert("Enter first name, second name, price");
+        return false;
+    } else if (firstName__input.value == "" && secondName__input.value == "" && age__input.value == "") {
+        alert("Enter first name, second name, age");
+        return false;
+    } else if (firstName__input.value == "" && secondName__input.value == "") {
+        alert("Enter first name, second name");
+        return false;
+    } else if (firstName__input.value == "" && age__input.value == "") {
+        alert("Enter first name, age")
+        return false;
+    } else if (firstName__input.value == "" && pricePerHourInDollars__input.value == "") {
+        alert("Enter first name, price");
+        return false;
+    } else if (secondName__input.value == "" && age__input.value == "") {
+        alert("Enter second name, age");
+        return false;
+    } else if (secondName__input.value == "" && pricePerHourInDollars__input.value == "") {
+        alert("Enter second name, price");
+        return false;
+    } else if (age__input.value == "" && pricePerHourInDollars__input.value == "") {
+        alert("Enter age, price");
+        return false;
+    } else if (firstName__input.value == "") {
+        alert("Enter first name");
+        return false;
+    } else if (secondName__input.value == "") {
+        alert("Enter second name");
+        return false;
+    } else if (age__input.value == "") {
+        alert("Enter age");
+        return false;
+    } else if (pricePerHourInDollars__input.value == "") {
+        alert("Enter price");
+        return false;
+    } else if (typeof pricePerHourInDollars__input.value !== "number") {
+        alert("Enter correct price");
+        return false;
+    } else {
+        return true;
     }
 }
 
 function editLawyer(index) {
     key = keys[index];
-    if (validation() === false) {
+    if (validation()) {
         firebase.database().ref("lawyers/" + key).set({
             firstName: firstName__input.value,
             secondName: secondName__input.value,
             age: age__input.value,
-            pricePerHourInDollars: pricePerHourInDollars__input.value
+            pricePerHourInDollars: pricePerHourInDollars__input.value,
+            id: index
         });
     } else {
-        alert("alert")
+        console.log("Data was not saved");
     }
 
     cleanInputs();
+    listOfLawyer = [];
+    loadFirebase();
 }
 
 function deleteLawyer(index) {
     key = keys[index]
     database.ref("lawyers/" + key).remove();
+    listOfLawyer = [];
+    loadFirebase();
 }
 
 searchList.addEventListener('keyup', (searchedString) => {
     const filterString = searchedString.target.value.toLowerCase();
-    let findLawyer = listOfLawyer.filter(lawyer => {
-        return lawyer.firstName.toLowerCase().includes(filterString);
-    });
-    lawyersTest = findLawyer;
+    priceCount = 0;
+    lawyersFinded = listOfLawyer.filter(lawyer => lawyer.firstName.toLowerCase().includes(filterString));
+    lawyersFinded.forEach(lawyer => { if (!isNaN(lawyer.pricePerHourInDollars)) priceCount += Number.parseInt(lawyer.pricePerHourInDollars) });
     cleanPage();
-    displayLawyers(lawyersTest);
+    displayLawyers(lawyersFinded);
 });
 
 clearBtn.addEventListener('click', () => {
     searchList.value = '';
-    lawyersTest = listOfLawyer;
-
+    listOfLawyer = [];
     loadFirebase();
 })

2886ef5f67e09bebb99091f18ec0e32d83892123
aekrops <svyatoslavtrostynski24@gmail.com>
2020-12-14 15:06:25 +0200

    rename lab8 to lab8-9 and added some functionality

---
+++ lab8-9/src/components/Catalog/ViewMoreButton.styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: #7ab1ff;
+  color: #e5f0ff;
+  border-style: none;
+  height: 30px;
+
+  &:hover {
+    background-color: #4a69ff;
+  }
+
+  &:active {
+    background-color: #2300b0;
+  }
+`;
